c28055edf423bf14283211855c0d739882d2253d
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2024-07-29 10:14:56 +0530

    Add files via upload

---
+++ src/components/NewsItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+let defaultUrl = "https://www.feednavigator.com/var/wrbm_gb_food_pharma/storage/images/9/2/8/5/235829-6-eng-GB/Feed-Test-SIC-Feed-20142.jpg";
+const NewsItem = (props) => {
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className="my-3">
+            <div className="card">
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    position: 'absolute',
+                    right: '0'
+                }
+                }>
+                    <span className="badge rounded-pill bg-danger"> {source} </span>
+                </div>
+                <img src={!imageUrl ? defaultUrl : imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}  </h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default NewsItem
